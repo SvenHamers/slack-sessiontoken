d4515ee2deeca192bb4969dec168f70fa87ec7de
SvenHamers <sven@qvision.nl>
2019-02-04 23:35:27 +0100

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Slack get sesion token
+
+
+
+Gets a session api key from slack. You can use this to speak to api function that returl invallid token like users.admin.setRegular
+
+
+you need to change the variables in main.go
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strings"
+	"bufio"
+	"os"
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+)
+
+const (
+	baseURL = "https://<slackurl>.slack.com"
+)
+
+var (
+	username = ""
+	password = ""
+)
+
+type App struct {
+	Client *http.Client
+}
+
+func (app *App) getCrumb() string {
+	loginURL := baseURL 
+	client := app.Client
+
+	response, err := client.Get(loginURL)
+
+	if err != nil {
+		log.Fatalln("Error fetching response. ", err)
+	}
+
+	defer response.Body.Close()
+
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal("Error loading HTTP response body. ", err)
+	}
+
+	token, _ := document.Find("input[name='crumb']").Attr("value")
+
+	
+
+	return token
+}
+
+func (app *App) GetToken() string {
+	
+	
+	client := app.Client
+	token := app.getCrumb()
+	
+
+	loginURL := baseURL 
+
+	data := url.Values{
+		"email":     {username},
+		"password":  {password},
+		"crumb": {token},
+		"signin": {"1"},
+		"redir": {""},
+		"has_remember": {"1"},
+		"remember": {"on"},
+
+	}
+
+	response, err := client.PostForm(loginURL, data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	pageContent := string(body)
+	bytes := []byte(pageContent)
+
+    err = ioutil.WriteFile("output.txt", bytes, 0644)
+    if err != nil {
+        panic(err)
+	}
+	
+	file, err := os.Open("output.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "api_token:"){
+			return strings.Split(scanner.Text(), "\"")[1]			
+		}     
+    }	
+	return ""
+}
+
+
+func main() {
+	jar, _ := cookiejar.New(nil)
+
+	app := App{
+		Client: &http.Client{Jar: jar},
+	}
+
+	token := app.GetToken()
+	fmt.Println(token);
+
+}
